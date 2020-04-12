2e527c4f52f3bf950e428a67ae2e5c2cf2178957
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-13 02:37:31 +0530

    image added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+        ImageView bart = (ImageView) findViewById(R.id.imageView);
+        bart.animate().alpha(0).setDuration(2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
